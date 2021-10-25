5c966cd3d8135aab2f9c2403db2d6e2cb1430498
DESKTOP-I8U1BFJ\AlexanderT <alextuzman@mail.ru>
2021-10-25 16:21:34 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/Molecules.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Molecules.dir/main.cpp.obj"
+  "Molecules.exe"
+  "Molecules.exe.manifest"
+  "Molecules.pdb"
+  "libMolecules.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Molecules.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Unit!" << std::endl;
+    return 0;
+}
